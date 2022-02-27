0c903319b2f89028ce8d71d02de178ec21ec91c1
HasanDevOps2003 <muhammedhasan483@gmail.com>
2022-02-27 02:19:52 +0000

    Lifecycle rule added to bucket

--- ex3-cicd-feature-env/main.tf
+++ ex3-cicd-feature-env/main.tf
@@ -6,6 +6,24 @@ resource "aws_s3_bucket" "TestBucket" {
     Name        = "My bucket"
     Environment = var.branch
   }
+  lifecycle_rule {
+    prefix  = "config/"
+    enabled = true
+
+    noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
 
   versioning {
     enabled = true
